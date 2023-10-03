209cc4ad20d18ba850ec0a95cced83b846584d92
madiha-waqar <mail2madihawaqar@gmail.com>
2023-10-03 10:40:39 -0400

    Set game, check scores and winners

---
+++ game.rb
@@ -0,0 +1,38 @@
+class Game
+  def initialize(p1, p2)
+    @player_1 = Player.new(p1, 3)
+    @player_2 = Player.new(p2, 3)
+  end
+
+  def start_game
+    puts "Welcome to math challenge! New game started."
+    turn
+  end
+
+  def declare_winner(player)
+    if player.lives > 1
+      puts "#{player.name} wins with #{player.lives} remaining lives."
+    else
+      puts "#{player.name} wins with 1 remaining life! Close one!"
+    end
+    
+    exit
+  end
+
+  def check_score
+    if @player_1.is_dead
+      declare_winner(@player_2)
+    elsif @player_2.is_dead
+      declare_winner(@player_1)
+    end
+  end
+
+  def turn
+    @player_1.new_question
+    check_score
+    @player_2.new_question
+    check_score
+    puts "Next round!"
+    turn
+  end
+end
